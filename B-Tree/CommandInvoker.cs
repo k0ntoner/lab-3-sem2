23406b7ed143665e1c0dab21f8d7c5d51a6c399f
k0ntoner <andrew4kot@gmail.com>
2024-05-24 15:38:01 +0300

    first commit

---
+++ B-Tree/CommandInvoker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public class CommandInvoker
+    {
+        private readonly Dictionary<string, ICommand> commands= new Dictionary<string, ICommand>();
+        public void RegisterCommand(string name, ICommand command)
+        {
+            commands[name]=command;
+        }
+        public void ExecuteCommand(string commandName)
+        {
+            if(commands.ContainsKey(commandName))
+            {
+                commands[commandName].Execute();
+            }
+        }
+    }
+}
---
+++ B-Tree/IBTree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_Tree
+{
+    /// <summary>
+    /// Інтерфейс для всіх BTree подовних дерев
+    /// </summary>
+    public interface IBTree
+    {
+        void Insert(int key);
+        void Delete(int key);
+        string Print();
+    }
+}
